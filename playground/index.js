f72d4548348a29ad491f10152c64226e4d4ca795
flippedround <734243792@qq.com>
2024-04-09 20:49:22 +0800

    ci: add ci file

--- playground/index.js
+++ playground/index.js
@@ -5,21 +5,26 @@ import {
   buttonRelease,
   keyPress,
   mouseMove,
+  listen
 } from '@rdev-js/core'
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-async function main() {
-  mouseMove(700, 900)
-  buttonPress(Button.Left)
-  await sleep(500)
-  buttonRelease(Button.Left)
-  await sleep(1000)
-  keyPress(Key.KeyA)
-  await sleep(500)
-  keyPress(Key.KeyA)
-}
+// async function main() {
+//   mouseMove(700, 900)
+//   buttonPress(Button.Left)
+//   await sleep(500)
+//   buttonRelease(Button.Left)
+//   await sleep(1000)
+//   keyPress(Key.KeyA)
+//   await sleep(500)
+//   keyPress(Key.KeyA)
 
-main()
+// }
+
+// main()
+listen((event) => {
+  console.log(event)
+})
